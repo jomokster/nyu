306ba9d6e88a149e7e436713bf8eb84fe96f1b81
joanne <>
2023-03-02 20:29:49 -0500

    some part of hw

--- JAMStack/_site/js/scripts.js
+++ JAMStack/_site/js/scripts.js
@@ -1,22 +1,24 @@
+// const API = "http://localhost:3000/results";
 
-const API = "http://localhost:3000/results";
+const API = "https://api.nytimes.com/svc/topstories/v2/nyregion.json?api-key=Tb5fBGclJuYZUydv6soIRa9ucureYf3m";
 
 
 function getStories(event) {
   fetch(API)
     .then((response) => response.json())
-    .then((data) => showData(data));
+    .then((data) => showData(data.results));
 }
 
-
 function showData(stories) {
+  stories.map((story) => console.log(story))
+
   var looped = stories
     .map(
       (story) => `
     <div class="item">
     <picture>
-    <img src="${story.multimedia[2].url}" alt="" />
-    <caption>${story.multimedia[2].caption}</caption>
+    <img src="${story.multimedia[0].url}" alt="" />
+    <figcaption>${story.multimedia[0].caption}</caption>
     </picture>
       <h3><a href="${story.url}">${story.title}</a></h3>
       <p>${story.abstract}</p>
